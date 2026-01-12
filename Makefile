4f060ea816add2412dbdbc33adcefe3a96c8bf43
Tim Hrovat <tim.hrovat@easistent.com>
2026-01-12 17:48:04 +0100

    Initial commit

---
+++ Makefile
@@ -0,0 +1,24 @@
+docs:
+	swag fmt
+	swag init --parseDependency -g api/api.go -o api/docs
+
+test:
+	godotenv go test ./...
+
+test-update:
+	godotenv go test ./... -update
+
+install-cli-tools:
+	go install github.com/joho/godotenv/cmd/godotenv@latest
+	go install -tags 'postgres' github.com/golang-migrate/migrate/v4/cmd/migrate@latest
+	go install github.com/swaggo/swag/cmd/swag@latest
+	go install github.com/go-swagger/go-swagger/cmd/swagger@latest
+
+migrate:
+	migrate -database postgres://postgres:postgres@localhost/predlogi?sslmode=disable -path db/migrations up
+
+migrate-down: 
+	migrate -database postgres://postgres:postgres@localhost/predlogi?sslmode=disable -path db/migrations down
+
+fixtures:
+	godotenv go run ../common/tools/loadfixture/loadfixture.go db/fixtures/
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"log/slog"
+	"os"
+
+	"github.com/PRPO-skupina-02/common/database"
+	"github.com/PRPO-skupina-02/common/logging"
+	"github.com/PRPO-skupina-02/common/validation"
+	"github.com/PRPO-skupina-02/predlogi/api"
+	"github.com/PRPO-skupina-02/predlogi/db"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	err := run()
+
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	slog.Info("Starting server")
+
+	logger := logging.GetDefaultLogger()
+	slog.SetDefault(logger)
+
+	db, err := database.OpenAndMigrateProd(db.MigrationsFS)
+	if err != nil {
+		return err
+	}
+
+	trans, err := validation.RegisterValidation()
+	if err != nil {
+		return err
+	}
+
+	router := gin.Default()
+	api.Register(router, db, trans)
+
+	slog.Info("Server startup complete")
+	err = router.Run(":8080")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
